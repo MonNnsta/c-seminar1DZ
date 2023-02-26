25561a177e3398024695cb931fc24c251f55a0d0
Kirill Plokhotnichenko <www.origin@bk.ru>
2023-02-26 21:46:30 +0500

    added Task6 solution

---
+++ Task6/Program.cs
@@ -0,0 +1,6 @@
+﻿Console.WriteLine("Введите число, а я скажу вам четное оно или нет :) ");
+int usernumber = Convert.ToInt32(Console.ReadLine());
+if (usernumber % 2 == 0)
+    Console.WriteLine("Ваше число четное! ");
+else
+    Console.WriteLine("Ваше число не четное! ");
