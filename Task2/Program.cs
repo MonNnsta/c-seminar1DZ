fbb4b3171ca4f2ba4ecb09699260244cdfbcad7f
Kirill Plokhotnichenko <www.origin@bk.ru>
2023-02-26 21:26:01 +0500

    added Task2 solution

--- Task2/Program.cs
+++ Task2/Program.cs
@@ -1,2 +1,14 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.WriteLine ("Введите 1е число");
+int usernumber1 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine ("Введите 2е число");
+int usernumber2 = Convert.ToInt32(Console.ReadLine());
+if (usernumber1 > usernumber2)
+{
+    Console.WriteLine("Большее число - " + usernumber1);
+    Console.WriteLine("Меньшее число - " + usernumber2);
+}
+else
+{
+    Console.WriteLine("Меньшее число - " + usernumber1);
+    Console.WriteLine("Большее число - " + usernumber2);
+}
