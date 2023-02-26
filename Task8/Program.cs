1d6034822994acc2d293d47e6eb3c4bb9d961727
Kirill Plokhotnichenko <www.origin@bk.ru>
2023-02-26 22:14:11 +0500

    added Task8 solution

---
+++ Task8/Program.cs
@@ -0,0 +1,8 @@
+﻿Console.WriteLine("Введите число, а я покажу вам все четные числа, которые были до этого числа! ");
+int usernumber = Convert.ToInt32(Console.ReadLine());
+int number = 2;
+while (number <= usernumber)
+{
+    Console.Write(number + "  ");
+    number = number + 2;}
+
