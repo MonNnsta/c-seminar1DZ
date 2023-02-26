7b234d65a235aaa038a1fb2844f4b03d96e2e49f
Kirill Plokhotnichenko <www.origin@bk.ru>
2023-02-26 21:37:47 +0500

    added Task4 solution

---
+++ Task4/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.WriteLine ("Введите 1е число");
+int usernumber1 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine ("Введите 2е число");
+int usernumber2 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine ("Введите 3е число");
+int usernumber3 = Convert.ToInt32(Console.ReadLine());
+if (usernumber1 > usernumber2 && usernumber1 > usernumber3)
+    Console.WriteLine ("Максимальное из введенных чисел = " + usernumber1);
+else if (usernumber2 > usernumber3)
+    Console.WriteLine ("Максимальное из введенных чисел = " + usernumber2);
+else
+    Console.WriteLine ("Максимальное из введенных чисел = " + usernumber3);
